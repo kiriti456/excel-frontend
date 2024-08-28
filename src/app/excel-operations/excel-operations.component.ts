05496f36510fc04aecf24343632085da24f442fd
kiriti456 <kiriti.1206@gmail.com>
2024-08-28 02:07:48 -0500

    updated working code

--- src/app/excel-operations/excel-operations.component.ts
+++ src/app/excel-operations/excel-operations.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import * as XLSX from 'xlsx';
-import { saveAs } from 'file-saver';
-import JSZip from 'jszip';
+import JSZip from 'jszip'; // Changed to default import
 
 @Component({
   selector: 'app-excel-operations',
@@ -11,8 +10,8 @@ import JSZip from 'jszip';
 export class ExcelOperationsComponent {
   file1: File | null = null;
   file2: File | null = null;
+  splitFileData: File | null = null; // Renamed to avoid conflict
   mergeColumnName: string = '';
-  file: File | null = null;
   splitColumnName: string = '';
 
   onFile1Selected(event: any) {
@@ -24,145 +23,147 @@ export class ExcelOperationsComponent {
   }
 
   onFileSelected(event: any) {
-    this.file = event.target.files[0];
+    this.splitFileData = event.target.files[0];
   }
 
-  mergeFiles() {
-    if (this.file1 && this.file2 && this.mergeColumnName) {
-      const file1Reader = new FileReader();
-      const file2Reader = new FileReader();
-
-      file1Reader.onload = (e: any) => {
-        const workbook1 = XLSX.read(e.target.result, { type: 'array' });
-        const sheet1 = workbook1.Sheets[workbook1.SheetNames[0]];
-        const data1 = XLSX.utils.sheet_to_json(sheet1, { header: 1 });
-
-        file2Reader.onload = (e: any) => {
-          const workbook2 = XLSX.read(e.target.result, { type: 'array' });
-          const sheet2 = workbook2.Sheets[workbook2.SheetNames[0]];
-          const data2 = XLSX.utils.sheet_to_json(sheet2, { header: 1 });
-
-          // Merge data
-          const { unionData, intersectionData } = this.mergeData(data1, data2, this.mergeColumnName);
-
-          // Create and download the resulting files
-          const zip = new JSZip();
-          zip.file('union.xlsx', this.createExcelBlob(unionData, 'Union'));
-          zip.file('intersection.xlsx', this.createExcelBlob(intersectionData, 'Intersection'));
-
-          zip.generateAsync({ type: 'blob' }).then(content => {
-            saveAs(content, 'merged_intersected_files.zip');
-          });
-        };
-
-        if (this.file2) {
-          file2Reader.readAsArrayBuffer(this.file2);
-        }
-      };
-
-      if (this.file1) {
-        file1Reader.readAsArrayBuffer(this.file1);
-      }
-    } else {
-      alert('Please select two files and provide a column name.');
+  async mergeFiles() {
+    if (!this.file1 || !this.file2 || !this.mergeColumnName) {
+      alert('Please upload two files and specify a column name.');
+      return;
     }
+
+    const data1 = await this.readExcelFile(this.file1);
+    const data2 = await this.readExcelFile(this.file2);
+
+    const columnIdx1 = this.getColumnIndex(data1[0], this.mergeColumnName);
+    const columnIdx2 = this.getColumnIndex(data2[0], this.mergeColumnName);
+
+    if (columnIdx1 === -1 || columnIdx2 === -1) {
+      alert('Column name not found in one or both files.');
+      return;
+    }
+
+    const { unionData, intersectionData } = this.mergeAndIntersect(data1, data2, columnIdx1, columnIdx2);
+
+    const unionBlob = this.createExcelBlob(unionData, 'Union');
+    const intersectionBlob = this.createExcelBlob(intersectionData, 'Intersection');
+
+    const zipBlob = await this.createZipFile(unionBlob, intersectionBlob);
+
+    this.downloadFile(zipBlob, 'merged_intersected_files.zip');
   }
 
-  splitFile() {
-    if (this.file && this.splitColumnName) {
-      const fileReader = new FileReader();
-
-      fileReader.onload = (e: any) => {
-        const workbook = XLSX.read(e.target.result, { type: 'array' });
-        const sheet = workbook.Sheets[workbook.SheetNames[0]];
-        const data = XLSX.utils.sheet_to_json(sheet, { header: 1 });
-
-        // Split data
-        const groupedData = this.splitData(data, this.splitColumnName);
-
-        // Create and download zip file
-        const zip = new JSZip();
-        Object.keys(groupedData).forEach(key => {
-          zip.file(`${key}.xlsx`, this.createExcelBlob(groupedData[key], key));
-        });
-
-        zip.generateAsync({ type: 'blob' }).then(content => {
-          saveAs(content, 'split_files.zip');
-        });
-      };
-
-      if (this.file) {
-        fileReader.readAsArrayBuffer(this.file);
-      }
-    } else {
-      alert('Please select a file and provide a column name.');
+  async splitExcelFile() { // Renamed the function
+    if (!this.splitFileData || !this.splitColumnName) {
+      alert('Please upload a file and specify a column name.');
+      return;
     }
+
+    const data = await this.readExcelFile(this.splitFileData);
+    const columnIdx = this.getColumnIndex(data[0], this.splitColumnName);
+
+    if (columnIdx === -1) {
+      alert('Column name not found in the file.');
+      return;
+    }
+
+    const splitFiles = this.splitByColumn(data, columnIdx);
+    const zipBlob = await this.createSplitZipFile(splitFiles);
+
+    this.downloadFile(zipBlob, 'split_files.zip');
   }
 
-  private mergeData(data1: any[], data2: any[], columnName: string) {
-    const header1 = data1[0];
-    const header2 = data2[0];
-    const index1 = header1.indexOf(columnName);
-    const index2 = header2.indexOf(columnName);
-  
-    if (index1 === -1 || index2 === -1) {
-      alert('Column name not found in one of the files.');
-      return { unionData: [], intersectionData: [] };
-    }
-  
-    const dataMap = new Map<string, any[]>();
-    data1.slice(1).forEach((row: any[]) => {
-      const key = this.normalizeKey(row[index1]);
-      dataMap.set(key, row);
-    });
-  
-    const intersectionData: any[][] = [header1];
-    const unionData: any[][] = [header1];
-  
-    data2.slice(1).forEach((row: any[]) => {
-      const key = this.normalizeKey(row[index2]);
-      if (dataMap.has(key)) {
-        const existingRow = dataMap.get(key);
-        const mergedRow = [...existingRow!];
-        row.forEach((value: any, i: number) => {
-          mergedRow[i] = value;
-        });
-        dataMap.set(key, mergedRow);
-        intersectionData.push(mergedRow);
+  private async readExcelFile(file: File): Promise<any[][]> {
+    const data = await file.arrayBuffer();
+    const workbook = XLSX.read(data, { type: 'array' });
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    return XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+  }
+
+  private getColumnIndex(headerRow: any[], columnName: string): number {
+    return headerRow.findIndex(header => this.normalizeKey(header) === this.normalizeKey(columnName));
+  }
+
+  private mergeAndIntersect(data1: any[][], data2: any[][], columnIdx1: number, columnIdx2: number) {
+    const headers1 = data1[0];
+    const headers2 = data2[0];
+
+    const allHeadersSet = new Set([...headers1, ...headers2]);
+    const allHeaders = Array.from(allHeadersSet);
+
+    const unionData = [allHeaders];
+    const intersectionData = [allHeaders];
+
+    const rowsMap1 = this.mapRowsByColumn(data1, columnIdx1, headers1);
+    const rowsMap2 = this.mapRowsByColumn(data2, columnIdx2, headers2);
+
+    const allKeys = new Set([...rowsMap1.keys(), ...rowsMap2.keys()]);
+
+    allKeys.forEach(key => {
+      const row1 = rowsMap1.get(key);
+      const row2 = rowsMap2.get(key);
+      const unionRow = this.createMergedRow(row1, row2, allHeaders, headers1, headers2);
+
+      unionData.push(unionRow);
+
+      if (row1 && row2) {
+        const intersectionRow = this.createMergedRow(row1, row2, allHeaders, headers1, headers2);
+        intersectionData.push(intersectionRow);
       }
-      unionData.push(row);
     });
-  
-    dataMap.forEach(row => {
-      if (!intersectionData.some(r => r[index1] === row[index1])) {
-        unionData.push(row);
-      }
-    });
-  
+
     return { unionData, intersectionData };
-  }  
-
-  private splitData(data: any[], columnName: string): { [key: string]: any[] } {
-    const header = data[0];
-    const index = header.indexOf(columnName);
-    if (index === -1) {
-      alert('Column name not found.');
-      return {};
-    }
-  
-    const groupedData: { [key: string]: any[] } = {};
-  
-    data.slice(1).forEach((row: any[]) => {
-      const key = this.normalizeKey(row[index]);
-      if (!groupedData[key]) {
-        groupedData[key] = [header];
-      }
-      groupedData[key].push(row);
-    });
-  
-    return groupedData;
   }
-  
+
+  private mapRowsByColumn(data: any[][], columnIndex: number, headers: any[]): Map<string, any[]> {
+    const rowMap = new Map<string, any[]>();
+    data.slice(1).forEach(row => {
+      const key = this.normalizeKey(row[columnIndex]);
+      rowMap.set(key, row);
+    });
+    return rowMap;
+  }
+
+  private createMergedRow(
+    row1: any[] | undefined,
+    row2: any[] | undefined,
+    allHeaders: any[],
+    headers1: any[],
+    headers2: any[]
+  ): any[] {
+    const mergedRow = new Array(allHeaders.length).fill('');
+
+    if (row1) {
+      headers1.forEach((header, i) => {
+        const colIdx = allHeaders.indexOf(header);
+        mergedRow[colIdx] = row1[i];
+      });
+    }
+
+    if (row2) {
+      headers2.forEach((header, i) => {
+        const colIdx = allHeaders.indexOf(header);
+        mergedRow[colIdx] = row2[i];
+      });
+    }
+
+    return mergedRow;
+  }
+
+  private splitByColumn(data: any[][], columnIdx: number): Map<string, any[][]> {
+    const headerRow = data[0];
+    const splitFiles = new Map<string, any[][]>();
+
+    data.slice(1).forEach(row => {
+      const key = this.normalizeKey(row[columnIdx]);
+      if (!splitFiles.has(key)) {
+        splitFiles.set(key, [headerRow]);
+      }
+      splitFiles.get(key)!.push(row);
+    });
+
+    return splitFiles;
+  }
 
   private normalizeKey(key: any): string {
     if (key === null || key === undefined) {
@@ -175,11 +176,35 @@ export class ExcelOperationsComponent {
     const workbook = XLSX.utils.book_new();
     const worksheet = XLSX.utils.aoa_to_sheet(data);
     XLSX.utils.book_append_sheet(workbook, worksheet, sheetName);
-    
-    // Convert workbook to array buffer
+
     const wbout = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
-  
+
     return new Blob([wbout], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
   }
-  
+
+  private async createZipFile(unionBlob: Blob, intersectionBlob: Blob): Promise<Blob> {
+    const zip = new JSZip(); // No error now
+    zip.file('union.xlsx', unionBlob);
+    zip.file('intersection.xlsx', intersectionBlob);
+    return await zip.generateAsync({ type: 'blob' });
+  }
+
+  private async createSplitZipFile(splitFiles: Map<string, any[][]>): Promise<Blob> {
+    const zip = new JSZip(); // No error now
+
+    splitFiles.forEach((data, key) => {
+      const blob = this.createExcelBlob(data, key);
+      zip.file(`${key}.xlsx`, blob);
+    });
+
+    return await zip.generateAsync({ type: 'blob' });
+  }
+
+  private downloadFile(blob: Blob, fileName: string) {
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = fileName;
+    link.click();
+    URL.revokeObjectURL(link.href);
+  }
 }
